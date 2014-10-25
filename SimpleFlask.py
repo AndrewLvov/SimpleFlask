66415e01bffbe8f916ac1e20788e683d8a00e63a
Andrey Yanov <andrey.cplusplus@gmail.com>
2014-10-25 10:23:26 +0300

    Added 'sum' form

--- SimpleFlask.py
+++ SimpleFlask.py
@@ -1,17 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+from random import randrange
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return """
-    <!DOCTYPE html>
-    <html>
-    <body>
-    <h1>Hello, {name}!</h1>
-    </body>
-    </html>
-    """.format(name='Петро')
+    numbers = [randrange(0, 100) for _ in range(10)]
+    return render_template('index.html', name="Кот Васька",
+                           numbers=numbers)
+
+
+@app.route('/sum/', methods=['GET', 'POST'])
+def sum_handler():
+    if request.method == 'POST':
+        a = request.form['a']
+        b = request.form['b']
+        return render_template('sum.html', a=a, b=b, total=int(a) + int(b))
+    else:
+        return render_template('sum.html')
 
 
 if __name__ == '__main__':
